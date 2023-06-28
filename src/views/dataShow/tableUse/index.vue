bb97f166605f8181b141d3fd89ff337649446ced
zhangyao <zhangyao220624@credithc.com>
2023-06-28 11:08:36 +0800

    feat: 文字溢出展示

--- src/views/dataShow/tableUse/index.vue
+++ src/views/dataShow/tableUse/index.vue
@@ -2,82 +2,82 @@
     <div class="table-box">
         <Mytable ref="myTable" :columns="columns" :requestApi="getUseTableList" :_options="options">
             <template v-slot:exportsO>
-				<slot name="exportsO">
-					<ElButton type="primary" :icon="Download" @click="handleClickExportBtn">导出</ElButton>
-				</slot>
-			</template>
+                <slot name="exportsO">
+                    <ElButton type="primary" :icon="Download" @click="handleClickExportBtn">导出</ElButton>
+                </slot>
+            </template>
             <template #tableHeader="scope">
-                <el-button type="primary" :icon="CirclePlus" @click="openDrawer('新增')" >新增用户</el-button>
+                <el-button type="primary" :icon="CirclePlus" @click="openDrawer('新增')">新增用户</el-button>
             </template>
         </Mytable>
     </div>
 </template>
 
 <script setup lang="ts">
-import {ref,reactive, onMounted} from "vue"
+import { ref, reactive, onMounted } from "vue"
 import Mytable from "@/components/Mytable/index.vue"
 import { getUseTableList } from "@/service/moudles/usetable/usetable"
-import { ElMessage,ElButton } from "element-plus"
-import { CirclePlus,Download } from "@element-plus/icons-vue";
+import { ElMessage, ElButton } from "element-plus"
+import { CirclePlus, Download } from "@element-plus/icons-vue";
 import { h } from 'vue'
 interface User {
-    username:string,
-    age:string,
-    phone:string,
-    userhobbyame:string,
+    username: string,
+    age: string,
+    phone: string,
+    userhobbyame: string,
 }
 // 表格设置
-const options:Table._options = {
-        pagination: true,
-        border: true,
-        stripe: false,
-        toolButton: true,
-        childrenName: "children"
+const options: Table._options = {
+    pagination: true,
+    border: true,
+    stripe: false,
+    toolButton: true,
+    childrenName: "children"
 }
-const columns:Partial<Table.ColumnProps>[] = [
-    { type: "selection", width: 80, fixed: "left",isTable:true,},
-    { type: "index", label: "序号", width: 80,isTable:true, },
-    {prop:'username',label:'姓名',align: "center",search: true,isTable:true,},
-    {prop:'age',label:'年龄',align: "center",search: true,isTable:true,},
-    {prop:'phone',label:'手机号',align: "center",search: true,isTable:true,},
-    {prop:'hobby',label:'爱好',align: "center",search: true,isTable:true,},
-    {prop:'hobby',label:'54645',align: "center",isTable:false,},
-    {prop:'hobby',label:'45645',align: "center",isTable:false,},
-    {prop:'hobby',label:'45654',align: "center",isTable:false,},
+const columns: Partial<Table.ColumnProps>[] = [
+    { type: "selection", width: 80, fixed: "left", isTable: true, },
+    { type: "index", label: "序号", width: 80, isTable: true, },
+    { prop: 'username', label: '姓名', align: "center", search: true, isTable: true, tooltip: true },
+    { prop: 'age', label: '年龄', align: "center", search: true, isTable: true, },
+    { prop: 'phone', label: '手机号', align: "center", search: true, isTable: true, },
+    { prop: 'hobby', label: '爱好', align: "center", search: true, isTable: true, },
+    { prop: 'hobby', label: '54645', align: "center", isTable: false, },
+    { prop: 'hobby', label: '45645', align: "center", isTable: false, },
+    { prop: 'hobby', label: '45654', align: "center", isTable: false, },
     {
-        width:140,
-        label:'操作',
-        render:(row:User,index:number) =>
+        width: 140,
+        label: '操作',
+        render: (row: User, index: number) =>
             h('div', null, [
-                    h(
-                        ElButton,
-                        {
-                            type: 'primary',
-                            size: 'small',
-                            onClick: () => handleRenderEdit(row, index)
-                        },
-                        { default: () => '编辑' }
-                    ),
-                    h(
-                        ElButton,
-                        {   
-                            type: 'danger',
-                            size: 'small',
-                            onClick: () => handleRenderDelete(row, index)
-                        },
-                        { default: () => '删除' }
-                    )
-                ])       
+                h(
+                    ElButton,
+                    {
+                        type: 'primary',
+                        size: 'small',
+                        onClick: () => handleRenderEdit(row, index)
+                    },
+                    { default: () => '编辑' }
+                ),
+                h(
+                    ElButton,
+                    {
+                        type: 'danger',
+                        size: 'small',
+                        onClick: () => handleRenderDelete(row, index)
+                    },
+                    { default: () => '删除' }
+                )
+            ])
     }
 ]
 
-const handleRenderEdit = (row:User, index:number) => {
+const handleRenderEdit = (row: User, index: number) => {
     ElMessage.success(`${row.username} ----- ${index}`)
 }
-const handleRenderDelete = (row:User, index:number) => {
+const handleRenderDelete = (row: User, index: number) => {
     ElMessage.success(`${row.username} ----- ${index}`)
 }
-const openDrawer =(title: string)=>{
+const openDrawer = (title: string) => {
     ElMessage.success("我是新增")
 }
 const handleClickExportBtn = () => {
